3cd56496ae050d5f61534f9420fc0dfce6dbaaa4
rmintz <rmintz@edgecast.com>
2024-11-29 16:06:59 -0500

    saving prog

---
+++ fe/vite-project/src/Message.tsx
@@ -0,0 +1,11 @@
+
+function Message() {
+    // JSX: javascript xml
+    const name = "shyguy"
+    if (name)
+        return <h1>Hello {name}</h1>
+    return <h1>Hello World</h1>;
+}
+
+export default Message;
+

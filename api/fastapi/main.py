c96f2c4b3c9ccbea39a8ba36ec017c26352c484c
rmintz <rmintz@edgecast.com>
2025-02-05 17:35:38 -0500

    session poc

--- api/fastapi/main.py
+++ api/fastapi/main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Cookie, Response, Request
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field
 import asyncio
 from fastapi.staticfiles import StaticFiles
-
+from session import session_layer
 
 app = FastAPI()
 
@@ -46,4 +47,30 @@ async def delayed_response(seconds: str = Query(...),
     
     await asyncio.sleep(seconds)
     return {"message": f"waited {seconds} seconds"}
+
+@app.get("/get_session")
+async def get_session(response: Response):
+    # make and set cookie
+    from datetime import datetime, timedelta
+    expiry = datetime.now() + timedelta(days=1)
+    cookie_value = session_layer.get_random_session_string()
+    response = JSONResponse(content={"this": "thatfakecontent"})
+    response.set_cookie(
+        key="prsm_session", 
+        value=cookie_value,
+        #expires=expiry,
+        httponly=True,
+        secure=True,
+        )
+    return response
+
+    # return session_layer.get_user_session()
+
+@app.get("/see_cookie")
+async def see_cookie(request: Request):
+    '''
+    Just print out the users cookie, for testing purposes
+    '''
+    return request.cookies.get('prsm_session')
+    
 
